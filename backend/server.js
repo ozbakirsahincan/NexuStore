10a9ac9041d6ccd55e0cfc41945c39042f412454
ozbakirsahincan <77192366+ozbakirsahincan@users.noreply.github.com>
2025-03-09 12:33:12 +0100

    CORS Update server.js
    Allowed host a göre apine gelen istekleri filtreleyebilirsin . örneğin sadece a şirketine verdiğin bir api olabilir.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
+// Frontend backend isteklerinde host kontrolü için
+const cors = require('cors');
+
 import productRoutes from "./routes/product.route.js"
 
 dotenv.config(); 
@@ -13,6 +16,17 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/products",productRoutes)
 
+// Allowed host tanımıyla sadece seni
+const allowedHosts = ['http://localhost:5173', ''];
+
+app.use((req, res, next) => {
+  const host = req.hostname;
+  if (allowedHosts.includes(host)) {
+    next();
+  } else {
+    res.status(403).send('Forbidden: Host not allowed');
+  }
+});
 
 // Sunucuyu başlatma
 app.listen(PORT, () => {
